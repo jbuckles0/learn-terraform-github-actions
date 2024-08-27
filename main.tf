5d3cc3387903aecda35f1514d162dbf1a82b4275
Jacob Buckles <124522813+jbuckles0@users.noreply.github.com>
2024-08-27 08:58:57 -0500

    remove server

--- main.tf
+++ main.tf
@@ -52,21 +52,6 @@ resource "aws_instance" "web" {
               EOF
 }
 
-resource "aws_instance" "new" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
